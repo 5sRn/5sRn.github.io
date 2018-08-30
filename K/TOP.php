2bab4e8e39e8a41f00163ea3cf98eae094de2b7a
5sRn <got-fc.1120@docomo.ne.jp>
2018-08-30 11:17:48 +0900

    コメット

---
+++ K/TOP.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>盛岡情報ビジネス専門学校-トップページ-</title>
+    <link rel="stylesheet" href="css/style2.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="inner">
+        <img src="images/header/logo.png">
+    </div>
+      </header>
+      <h3></h3>
+  <ul class = "foo">
+    <li><a href="TOP.php">トップ</a></li>
+    <li><a href="gakkou.php">学校紹介</a></li>
+    <li><a href="gakka.php">学科紹介</a></li>
+    <li><a href="nyugaku.php">入学案内</a></li>
+    <li><a href="syusyoku.php">就職実績</a></li>
+    <li><a href="sien.php">学生支援</a></li>
+  </ul>
+
+      <img src="images/oc/kv_gakka.jpg" alt="画像01" width="1300" height="490">
+<img src="images/oc/kv_syusyoku.gif" alt="画像02" width="1300" height="490">
+<img src="kv_gakkou.jpg" alt="画像03" width="1300" height="490">
+
+
+
+<footer>
+    <h3>盛岡情報ビジネス専門学校</h3>
+</footer>
+</body>
+</html>
